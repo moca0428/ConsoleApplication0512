7da29662802e948b8242a98e2ba7eb9b100f83ab
moca0428 <kdt25SGC40016@edu.kobe-tech.ac.jp>
2026-05-12 15:32:46 +0900

    これから他のstring変数を追加することを想定し、変数名をわかりやすく「greet」に変更

--- ConsoleApplication0512.cpp
+++ ConsoleApplication0512.cpp
@@ -9,17 +9,17 @@ int main()
     {
 
         //　string型の変数を作り、そこに"Hello World!"を入れる。
-        std::string s = "Hello World!";
+        std::string greet = "Hello World!";
 
         //  2回に1回（奇数回のみ逆転）
         //　iが今の回数なので、それを2で割ったあまりが1の時が奇数回。
         if (i % 2 == 1)
         {
             // 奇数回だけ反転（2回に1回）
-            std::reverse(s.begin(), s.end());
+            std::reverse(greet.begin(), greet.end());
         }
 
-        std::cout << s << "\n";
+        std::cout << greet << "\n";
 
     }
 }
