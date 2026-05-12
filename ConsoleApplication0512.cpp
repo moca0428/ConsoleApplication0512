a3a5ccfcffadd6366bc607bc6b2676ebf7e5fd88
moca0428 <kdt25SGC40016@edu.kobe-tech.ac.jp>
2026-05-12 15:27:23 +0900

    2回に1回、表示する文字列を反転させる処理に変更

--- ConsoleApplication0512.cpp
+++ ConsoleApplication0512.cpp
@@ -8,7 +8,18 @@ int main()
     for (int i = 0; i < 100; i++)
     {
 
-        std::cout << "Hello World!\n";
+        //　string型の変数を作り、そこに"Hello World!"を入れる。
+        std::string s = "Hello World!";
+
+        //  2回に1回（奇数回のみ逆転）
+        //　iが今の回数なので、それを2で割ったあまりが1の時が奇数回。
+        if (i % 2 == 1)
+        {
+            // 奇数回だけ反転（2回に1回）
+            std::reverse(s.begin(), s.end());
+        }
+
+        std::cout << s << "\n";
 
     }
 }
